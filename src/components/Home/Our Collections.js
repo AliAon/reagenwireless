97d3f3799401ac18eb09d6dba31ad4732dfdb884
aon <aon094944@gmail.com>
2023-01-25 16:16:02 +0500

    collection section done desktop

--- src/components/Home/Our Collections.js
+++ src/components/Home/Our Collections.js
@@ -13,10 +13,36 @@ const OurCollections = () => {
       />
       <Row>
         <Col lg={6}>
-
+          <div className="collection">
+            <img
+              src="./images/collection/placeholder.png"
+              className="collection__img"
+            />
+            <div className="collection__detail">
+              <h4 className="collection__detail__title">
+                Gaming PC Collection
+              </h4>
+              <p className="collection__detail__description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod.
+              </p>
+            </div>
+          </div>
         </Col>
         <Col lg={6}>
-  
+          <div className="collection">
+            <img
+              src="./images/collection/placeholder (1).png"
+              className="collection__img"
+            />
+            <div className="collection__detail">
+              <h4 className="collection__detail__title">Camera Collection</h4>
+              <p className="collection__detail__description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod.
+              </p>
+            </div>
+          </div>
         </Col>
       </Row>
     </div>
